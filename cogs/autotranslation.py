c17fd2c78ec74f58457ecfb6eb71139f39d11bf8
Epic <admin@itzepic.net>
2020-07-23 19:38:05 +0200

    Feat: Add automatic translations

---
+++ cogs/autotranslation.py
@@ -0,0 +1,35 @@
+"""
+Created by Epic at 7/2/20
+"""
+import logging
+
+import discord
+import googletrans
+from discord.ext import commands
+
+
+class AutoTranslation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger("salbot.cogs.autotranslation")
+        self.translator = googletrans.Translator()
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if not message.guild:
+            return
+        if message.author.bot:
+            return
+        detected = self.translator.detect(message.content)
+        if detected.confidence < .5:
+            return
+
+        translated = self.translator.translate(message.content)
+        if translated.src == "en" or translated.text == message.content:
+            return
+        await message.channel.send(translated.text)
+
+
+def setup(bot):
+    bot.add_cog(AutoTranslation(bot))
+
